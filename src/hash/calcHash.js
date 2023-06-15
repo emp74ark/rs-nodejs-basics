a04a9cb31ec28d75d16dc8985169e65f817056f3
emp74ark <emp74ark@gmail.com>
2023-06-15 12:49:13 +0200

    feat: hash subtask

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import {dirname, resolve} from 'path';
+import {readFile} from 'fs/promises';
+import {createHash} from 'crypto';
+import {fileURLToPath} from 'url';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const file = resolve(dir, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const data = await readFile(file);
+    const hash = createHash('sha256').update(data).digest('hex');
+    console.log(hash);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await calculateHash();
