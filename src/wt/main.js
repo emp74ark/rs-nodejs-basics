d8f7452764fd11f9f60754c02d076bde6be440b8
emp74ark <emp74ark@gmail.com>
2023-06-15 20:19:49 +0200

    feat: threads subtask

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,34 @@
+import {cpus} from 'os';
+import {dirname, resolve} from 'path';
+import {fileURLToPath} from 'url';
+import {Worker} from 'worker_threads';
+
+const threads = cpus().length;
+const fibonacci = resolve(dirname(fileURLToPath(import.meta.url)), 'worker.js');
+
+function resultDecorator(result) {
+  const arr = []
+  for (const r of result) {
+    const status = r.status === 'fulfilled' ? 'resolved' : 'error';
+    const value = r.status === 'fulfilled' ? r.value : null;
+    arr.push(`status: ${status}, data: ${value}`)
+  }
+  console.log(arr);
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  const promises = [];
+  for (let i = 0; i < threads; i++) {
+    const promise = new Promise((resolve, reject) => {
+      const worker = new Worker(fibonacci, {workerData: i + 10});
+      worker.on('message', resolve);
+      worker.on('error', reject);
+    });
+    promises.push(promise);
+  }
+  Promise.allSettled(promises).then(result => {
+    resultDecorator(result)
+  });
 };
 
 await performCalculations();
