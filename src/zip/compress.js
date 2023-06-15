b894079d7311794d510d288a5fb020500c560f19
emp74ark <emp74ark@gmail.com>
2023-06-15 14:07:58 +0200

    feat: zlib subtask

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import {dirname, resolve} from 'path';
+import {fileURLToPath} from 'url';
+import {createGzip} from 'zlib';
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream/promises';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const source = resolve(dir, 'files', 'fileToCompress.txt');
+const target = resolve(dir, 'files', 'archive.gz');
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const rs = createReadStream(source);
+  const ws = createWriteStream(target);
+  try {
+    await pipeline(rs, gzip, ws);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import {dirname, resolve} from 'path';
+import {fileURLToPath} from 'url';
+import {createGunzip} from 'zlib';
+import {createReadStream, createWriteStream} from 'fs';
+import {pipeline} from 'stream/promises';
+
+const dir = dirname(fileURLToPath(import.meta.url));
+const source = resolve(dir, 'files', 'archive.gz');
+const target = resolve(dir, 'files', 'uncompressed_fileToCompress.txt');
 const decompress = async () => {
-    // Write your code here 
+  const gunzip = createGunzip();
+  const rs = createReadStream(source);
+  const ws = createWriteStream(target);
+  try {
+    await pipeline(rs, gunzip, ws);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
 await decompress();
